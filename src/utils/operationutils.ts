28fd2566b40e19bba911ab4ff3a7f3f80466e52d
Ivan Garcia <ivanggv2@gmail.com>
2023-02-11 18:25:06 +0100

    modified date to have the local time system

--- src/utils/operationutils.ts
+++ src/utils/operationutils.ts
@@ -1,5 +1,7 @@
 export const calculateMinutes = (loginDate: Date) => {
-  const diff = (loginDate.getTime() - new Date().getTime()) / 1000;
+  const diff =
+    (loginDate.getTime() - new Date(new Date().toLocaleString()).getTime()) /
+    1000;
 
   return Math.abs(Math.round(diff / 60));
 };
