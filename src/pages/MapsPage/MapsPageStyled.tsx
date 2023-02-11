8bc390f2196438d018a7808d6db66ac04f45f85d
Ivan Garcia <ivanggv2@gmail.com>
2023-02-11 18:08:52 +0100

    added component LeafletMap and styles

---
+++ src/pages/MapsPage/MapsPageStyled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const MapsPageStyled = styled.div`
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+
+  .leaflet-container {
+    width: 800px;
+    height: 800px;
+  }
+`;
+
+export default MapsPageStyled;
