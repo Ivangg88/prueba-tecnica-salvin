05c00ba9c2f3a4667f1e99d765c8803822faec1c
Ivan Garcia <ivanggv2@gmail.com>
2023-02-11 13:15:14 +0100

    created component LoginFormikForm

---
+++ src/components/LoginFormikForm/LoginFormikForm.tsx
@@ -0,0 +1,49 @@
+import { ErrorMessage, Field, Form, useFormikContext } from "formik";
+
+const LoginFormikForm = (): JSX.Element => {
+  const { isValid } = useFormikContext();
+
+  return (
+    <Form
+      noValidate
+      autoComplete="off"
+      className="form-container"
+      data-testid="loginForm"
+    >
+      <Field
+        class="form-container__input"
+        label="Username"
+        name="userName"
+        placeholder="username"
+        type="text"
+      />
+      <ErrorMessage name="userName">
+        {(msg) => (
+          <span data-testid="errorMessage" className="errors">
+            {msg}
+          </span>
+        )}
+      </ErrorMessage>
+
+      <Field
+        class="form-container__input"
+        label="Password"
+        name="password"
+        placeholder="********"
+        type="password"
+      />
+      <ErrorMessage name="password">
+        {(msg) => (
+          <span data-testid="errorMessage" className="errors">
+            {msg}
+          </span>
+        )}
+      </ErrorMessage>
+
+      <button type="submit" disabled={!isValid}>
+        Login
+      </button>
+    </Form>
+  );
+};
+export default LoginFormikForm;
