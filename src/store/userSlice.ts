a0fc05a8ac763ac7285c4b38ed125dd7c7568d3e
Ivan Garcia <ivanggv2@gmail.com>
2023-02-11 13:01:01 +0100

    created hook useUser and the slice for user

---
+++ src/store/userSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserLoged } from "../types/interfaces";
+import { initialLoggedUser } from "../utils/initialStates";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: initialLoggedUser,
+  reducers: {
+    loginUser: (previousState, action: PayloadAction<UserLoged>) => ({
+      ...action.payload,
+    }),
+    logoutUser: (previousState) => initialLoggedUser,
+  },
+});
+
+export const userReducer = userSlice.reducer;
+
+export const {
+  loginUser: loginUserActionCreator,
+  logoutUser: logoutUserActionCreator,
+} = userSlice.actions;
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,3 +1,17 @@
-export interface Example {
-  example: string;
+export interface Userlogin {
+  userName: string;
+  password: string;
+}
+
+export interface UserLoged {
+  userName: string;
+  token: string;
+  isLogged: boolean;
+  timeStamp: Date;
+}
+
+export interface Token {
+  userName: string;
+  timeStamp: Date;
+  isLogged: boolean;
 }
