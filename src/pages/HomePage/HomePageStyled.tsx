d85eba0a9ed48cd2bfaa16047d7b7f4939501700
Ivan Garcia <ivanggv2@gmail.com>
2023-02-11 19:29:25 +0100

    added content to home page

---
+++ src/pages/HomePage/HomePageStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HomePageStyled = styled.div`
+  width: 100%;
+  padding: 20px;
+
+  span > span {
+    font-weight: bold;
+  }
+`;
+
+export default HomePageStyled;
