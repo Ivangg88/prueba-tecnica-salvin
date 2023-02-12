8a1c2b381941e46114d69586e116be290183f4ee
Ivan Garcia <ivanggv2@gmail.com>
2023-02-12 11:34:55 +0100

    added test for calculateMinutes

---
+++ src/utils/operationUtils/operationUtils.test.ts
@@ -0,0 +1,25 @@
+import { calculateMinutes } from "./operationutils";
+
+describe("Given a function calculateMinutes", () => {
+  const actualDate = new Date();
+  describe("When is called with the actual date", () => {
+    test("Then it should return 0", () => {
+      const expectedResult = 0;
+      const receiveResult = calculateMinutes(
+        new Date(actualDate.toLocaleString())
+      );
+
+      expect(receiveResult).toBe(expectedResult);
+    });
+  });
+
+  describe("When is called with a date", () => {
+    test("Then it should return the diference respect to the actual date", () => {
+      const testDate = "12/2/2023, 11:20:56";
+
+      const receiveResult = calculateMinutes(new Date(testDate));
+
+      expect(receiveResult).not.toBe(0);
+    });
+  });
+});
--- src/utils/operationUtils/operationutils.ts
+++ src/utils/operationUtils/operationutils.ts
@@ -1,7 +1,8 @@
 export const calculateMinutes = (loginDate: Date) => {
+  debugger;
   const diff =
     (loginDate.getTime() - new Date(new Date().toLocaleString()).getTime()) /
     1000;
-
+  debugger;
   return Math.abs(Math.round(diff / 60));
 };
