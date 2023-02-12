6d4c2b96336b570e2265291b1345261abb68449f
Ivan Garcia <ivanggv2@gmail.com>
2023-02-12 10:10:04 +0100

    added ToastContainer and added custom style

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,31 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import HomePage from "../../pages/HomePage/HomePage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import MapsPage from "../../pages/MapsPage/MapsPage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import CredentialRoutes from "../CredentialRoute/CredentialRoute";
 import Layout from "../Layout/Layout";
+import "./App.css";
 
 const App = (): JSX.Element => {
   return (
     <>
+      <ToastContainer
+        bodyClassName="toast-body"
+        toastClassName="toast"
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+      />
       <Layout>
         <Routes>
           <Route path="/" element={<Navigate to={"/home"} />} />
