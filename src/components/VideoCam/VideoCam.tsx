7733c0cf6fe985ad83c33e1c2d01ea0b06de1149
Ivan Garcia Guerrero <igarcia@salvi.local>
2023-03-13 12:47:46 +0100

    add dragable version

--- src/components/VideoCam/VideoCam.tsx
+++ src/components/VideoCam/VideoCam.tsx
@@ -5,6 +5,26 @@ import { VideoCamStyled } from "./VideoCamStyled";
 export const VideoCam = () => {
   const [showVideo, setShowVideo] = useState(false);
 
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+
+  const [origin, setOrigin] = useState({ x: 0, y: 0 });
+  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    const startX = event.clientX - position.x;
+    const startY = event.clientY - position.y;
+    setOrigin({ x: startX, y: startY });
+  };
+  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
+    event.currentTarget.style.setProperty("display", "none");
+  };
+
+  const handleDragEnd = (event: React.DragEvent<HTMLDivElement>) => {
+    const x = event.clientX - origin.x;
+    const y = event.clientY - origin.y;
+    event.currentTarget.style.setProperty("display", "flex");
+    console.log(x, y);
+    setPosition({ x, y });
+  };
+
   return (
     <VideoCamStyled>
       {!showVideo && (
@@ -12,32 +32,27 @@ export const VideoCam = () => {
           className="custom-button"
           onClick={() => setShowVideo(!showVideo)}
         >
-          <AiOutlineVideoCamera className="icon" />{" "}
+          <AiOutlineVideoCamera />{" "}
         </button>
       )}
       {showVideo && (
         <div
-          // onDragEnd={(event) => {
-          //   console.log("Bottom", event.currentTarget.getBoundingClientRect());
-          //   console.log("Native element", event.nativeEvent);
-
-          //   event.currentTarget.style.setProperty(
-          //     "left",
-          //     (event.nativeEvent.x + event.currentTarget.x).toString() + "px"
-          //   );
-          //   event.currentTarget.style.setProperty(
-          //     "left",
-          //     (event.nativeEvent.y + 20).toString() + "px"
-          //   );
-          // }}
-          //draggable={true}
           className="video-container"
+          onDragStart={handleDragStart}
+          onDrag={handleDrag}
+          onDragEnd={handleDragEnd}
+          draggable={true}
+          style={{
+            position: "absolute",
+            left: `${position.x}px`,
+            top: `${position.y}px`,
+          }}
         >
           <button
             className="custom-button"
             onClick={() => setShowVideo(!showVideo)}
           >
-            <AiFillCloseCircle className="icon" />
+            <AiFillCloseCircle />
           </button>
           <div className="video"></div>
         </div>
